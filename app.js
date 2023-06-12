34b986899fd0e592ec2731f9cc19e2797728c305
snappyjel <leviftr1@gmail.com>
2023-06-12 20:46:27 +0700

    Create operate function

---
+++ app.js
@@ -0,0 +1,34 @@
+const num1 = 0;
+const operator = "+";
+const num2 = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Invalid operator";
+  }
+}
