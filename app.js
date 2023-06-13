f62af122a3b6a2876eadfd0d85cccaaedeaf31bb
snappyjel <leviftr1@gmail.com>
2023-06-13 21:41:04 +0700

    TODO

--- app.js
+++ app.js
@@ -14,6 +14,49 @@ function divide(a, b) {
   return a / b;
 }
 
+const equalSign = document.querySelectorAll(".equal-sign");
+const calcbody = document.querySelectorAll(".calculator-body");
+let resultText = document.querySelector(".result");
+
+const num1 = 0;
+const operator = "+";
+const num2 = 0;
+
+const numeric = document.querySelectorAll(".numeric");
+numeric.forEach((button) => {
+  button.addEventListener("click", () => {
+    resultText.textContent += button.value;
+  });
+});
+
+const opSymbol = document.querySelectorAll(".operator");
+opSymbol.forEach((button) => {
+  button.addEventListener("click", () => {
+    resultText.textContent += ` ${button.value} `;
+  });
+});
+
+const clearAll = document.querySelector("#clear-all");
+clearAll.addEventListener("click", clearCalculator);
+
+function clearCalculator() {
+  resultText.textContent = " ";
+}
+
+equalSign.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Get the values of num1, operator, and num2 from the resultText element
+    const [num1, operator, num2] = resultText.textContent.split(" ");
+
+    // Convert num1 and num2 to numbers
+    const num1Value = parseFloat(num1);
+    const num2Value = parseFloat(num2);
+
+    const result = operate(operator, num1Value, num2Value);
+    resultText.textContent = result;
+  });
+});
+
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
@@ -28,21 +71,3 @@ function operate(operator, num1, num2) {
       return "Invalid operator";
   }
 }
-
-const numeric = document.querySelectorAll(".numeric");
-const opsymbol = document.querySelectorAll(".operator");
-const equalsign = document.querySelectorAll(".equal-sign");
-const calcbody = document.querySelectorAll(".calculator-body");
-let resultText = document.querySelector(".result");
-const num1 = 0;
-const operator = "+";
-const num2 = 0;
-
-equalsign.forEach((button) => {
-  button.addEventListener("click", () => {
-    // TODO CONNECT FUNCTION TO GET NUMBERS
-
-    const result = operate(operator, num1Value, num2Value);
-    resultText.textContent = result;
-  });
-});
